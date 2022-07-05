1e12167e2f615aa582c9912bcea044d8ad38ad57
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-07-05 17:40:06 -0500

    homework start up added, header bar, and search form added

---
+++ Assets/script.js
@@ -0,0 +1,34 @@
+//SEARCH
+// grabbing values from search, to later use in our functions
+function searchHandle() {
+    //grab the search content
+    fetchLocation()
+}
+//FUNCTIONALITY FOR FETCHING LOCATION//
+//activity 3
+//call the location api
+
+function fetchLocation(search) {
+//activity 3. get some data pertaining to location - using api call
+// fetchweather(data we received from fetchLocation)
+}
+
+//FUNCTIONALITY FOR FETCHING WEATHER//
+
+function fetchWeather (location) {
+// get some data for the weather in the lcoation we search using an api call
+//call it
+
+renderWeather(data)
+}
+
+//FUNCTIONALITY FOR RENDERING CONTENT
+function renderWeather(data) {
+//render the weather
+}
+data.temp
+data.wind
+data.humidity
+data.uv
+//HISTORY FEATURE
+//local storage
