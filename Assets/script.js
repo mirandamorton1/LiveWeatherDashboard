6c41a9226b421c4514d2f587216d5bef34ad77a3
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-07-06 16:04:03 -0500

    making new JS page after tutoring.

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,19 @@
+var currentDate = moment().format("MMMM Do YYYY")
+// var urlRequest = '"http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=Imperial" + "&APPID=c10bb3bd22f90d636baa008b1529ee25"'
+var userInput = $('.userInput').val()
+// console.log(userInput) //this works
 //SEARCH
 // grabbing values from search, to later use in our functions
+
+$('.searchBtn').click(function (event){
+    // console.log('user clicked');
+    event.preventDefault();
+
+    searchedCity.text(userInput + " " + currentDate)
+    fetchLocation(userInput);
+})
+
+
 function searchHandle() {
     //grab the search content
     fetchLocation()
@@ -8,10 +22,26 @@ function searchHandle() {
 //activity 3
 //call the location api
 
-function fetchLocation(search) {
+function fetchLocation(userInput) {
+    console.log(userInput);
+    fetch('"https://api.openweathermap.org/data/2.5/onecall?lat="${lat}"&lon="${lon}"&exclude="${part}"&appid="${77d064324a88c3f01d3e55ce2b602b2c}"')
+    // fetch("https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial&appid=c10bb3bd22f90d636baa008b1529ee25")
+    var urlRequest = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial&appid=c10bb3bd22f90d636baa008b1529ee25";
+    $.ajax({
+        url:urlRequest,
+        method:"GET",
+    })
+
+        .then(function (data) {
+            console.log(data)
+
+        })
+
+    }
+    fetchLocation(userInput);
 //activity 3. get some data pertaining to location - using api call
 // fetchweather(data we received from fetchLocation)
-}
+
 
 //FUNCTIONALITY FOR FETCHING WEATHER//
 
@@ -26,9 +56,9 @@ renderWeather(data)
 function renderWeather(data) {
 //render the weather
 }
-data.temp
-data.wind
-data.humidity
-data.uv
+// data.temp
+// data.wind
+// data.humidity
+// data.uv
 //HISTORY FEATURE
 //local storage
